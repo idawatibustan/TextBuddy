fcfb6c2a683f3509b72d0c49811e961a540130c8
Idawati Bustan <idawatibustan@users.noreply.github.com>
2015-09-18 22:26:27 +0800

    Create command class object

--- src/TextBuddy.java
+++ src/TextBuddy.java
@@ -5,6 +5,18 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 
+public class Command{
+	public String type;
+	public String detail;
+	
+	public Command(String type, String detail){
+		this.type = type;
+		this.detail = detail;
+	}
+	
+}
+
+
 public class TextBuddy{
 	//attributes
 	public String fileName;
@@ -12,24 +24,23 @@ public class TextBuddy{
 	public int numEntry;
 	
 	//constructor
-	public TextBuddy(String name){
+	public TextBuddy(String name) {
 		this.fileName = name;
 		this.file = new File(name);
 		try{
 			if(!file.exists()){
 				file.createNewFile(); //create if file not exist
 			}
-		} catch (IOException e){
-			e.printStackTrace();
+		} catch (IOException e){ // there is an error in creating the file
+			e.printStackTrace(); // log the error
 		}
-		
 		this.numEntry = 0;
-	};
+	}
 	
 	//messages constants
 	public static final String MESSAGE_WELCOME = "Welcome to TextBuddy. ";
 	public static final String MESSAGE_FILE_READY= " is ready for use";
-	public static final String MESSAGE_COMMAND = "command: \n";
+	public static final String MESSAGE_COMMAND = "command: ";
 	public static final String MESSAGE_ADD = "added to ";
 	public static final String MESSAGE_DELETE = "deleted from ";
 	public static final String MESSAGE_CLEAR = "all content deleted from ";
@@ -37,53 +48,79 @@ public class TextBuddy{
 	
 	private static Scanner sc = new Scanner(System.in);
 	
-	public static void main(String[] args) throws IOException{
-		TextBuddy tb1 = new TextBuddy(args[0]);
+	public static void main(String[] args) throws IOException {
+		TextBuddy tb = new TextBuddy(args[0]);
+		toUser(MESSAGE_WELCOME + tb.fileName + MESSAGE_FILE_READY);
 		
-		toUser(MESSAGE_WELCOME + tb1.fileName + MESSAGE_FILE_READY);
-		
-		while(true){ 
-			System.out.print(MESSAGE_COMMAND);
-			String commandLine = sc.nextLine();
-			switch(getFirstWord(commandLine)){
-			case "add":
-				doAdd(removeFirstWord(commandLine), tb1.file, tb1.fileName); break;
-			case "display":
-				int count = doDisplay(tb1.file); 
-				if(count == 0){toUser(tb1.fileName + MESSAGE_EMPTY);} break;
-			case "delete":
-				
-				String content = doDelete(Integer.valueOf(removeFirstWord(commandLine)), tb1.file); 
-				toUser(MESSAGE_DELETE + tb1.fileName + ": \"" + content + "\""); break;
-			case "clear":
-				doClear(tb1.file); toUser(MESSAGE_CLEAR + tb1.fileName); break;
-			case "exit":
-				System.exit(0);
-			default: toUser("Command Error!");
-			}
+		while(true){
+			toUser(MESSAGE_COMMAND);
+			executeCommand(sc.nextLine(), tb);
+//			String commandLine = sc.nextLine();
+//			switch(getFirstWord(commandLine)){
+//			case "add":
+//				doAdd(removeFirstWord(commandLine), tb1.file, tb1.fileName); break;
+//			case "display":
+//				int count = doDisplay(tb1.file); 
+//				if(count == 0){toUser(tb1.fileName + MESSAGE_EMPTY);} break;
+//			case "delete":
+//				
+//				String content = doDelete(Integer.valueOf(removeFirstWord(commandLine)), tb1.file); 
+//				toUser(MESSAGE_DELETE + tb1.fileName + ": \"" + content + "\""); break;
+//			case "clear":
+//				doClear(tb1.file); toUser(MESSAGE_CLEAR + tb1.fileName); break;
+//			case "exit":
+//				System.exit(0);
+//			default: toUser("Command Error!");
+//			}
 		}
 	}
 	
+	//print to user
 	public static void toUser(String msg){
 		System.out.println(msg);
 	}
+
+	//execute relevant command
+	public static void executeCommand(String commandLine, TextBuddy tb) throws IOException{
+		Command cmd = new Command(getFirstWord(commandLine), removeFirstWord(commandLine));
+		switch(cmd.type){
+		case "add":
+			doAdd(cmd.detail, tb); break;
+//		case "display":
+//			int count = doDisplay(tb1.file); 
+//			if(count == 0){toUser(tb1.fileName + MESSAGE_EMPTY);} break;
+//		case "delete":			
+//			String content = doDelete(Integer.valueOf(removeFirstWord(commandLine)), tb1.file); 
+//			toUser(MESSAGE_DELETE + tb1.fileName + ": \"" + content + "\""); break;
+//		case "clear":
+//			doClear(tb1.file); toUser(MESSAGE_CLEAR + tb1.fileName); break;
+//		case "exit":
+//			System.exit(0);
+//		default: toUser("Command Error!");
+		//TODO
+		}
+	}
 	
+	//get first word: command word
 	public static String getFirstWord(String string){
 		return string.trim().split("\\s+")[0];
 	}
 	
+	//remove first word: command word
 	public static String removeFirstWord(String string){
 		return string.replace(getFirstWord(string),"").trim();
 	}
 	
-	public static void doAdd(String content, File file, String fileName) throws IOException{
-		FileWriter fw = new FileWriter(file, true);
+	//execute command: add
+	public static void doAdd(String content, TextBuddy tb) throws IOException{
+		FileWriter fw = new FileWriter(tb.file, true);
 		fw.write(content);
 		fw.write(System.lineSeparator());
 		fw.close();
-		toUser("added to " + fileName + ": \"" + content + "\"");
+		toUser("added to " + tb.fileName + ": \"" + content + "\"");
 	}
 	
+	//execute command: display
 	public static int doDisplay(File file) throws IOException{
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		int count = 0;
@@ -96,6 +133,7 @@ public class TextBuddy{
 		return count;
 	}
 	
+	//execute command: delete
 	public static String doDelete(Integer line, File file) throws IOException{
 		File temp = new File("temp.txt");
 		try{
@@ -140,7 +178,7 @@ public class TextBuddy{
 		return content;
 	}
 
-	
+	//execute command: clear
 	public static void doClear(File file) throws IOException{
 		FileWriter fwo = new FileWriter(file, false);
 		fwo.write("");
