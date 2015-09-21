68c2b15207d8ac536a9cd35c04d0f25f064a9839
Idawati Bustan <idawatibustan@users.noreply.github.com>
2015-09-21 23:33:05 +0800

    Create JUnit Test Case

---
+++ src/TextBuddyTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TextBuddyTest {
+
+	@Test
+	public void listShouldBeSorted() {
+		
+		//TextBuddy to be tested
+		fail("Not yet implemented");
+	}
+
+}
